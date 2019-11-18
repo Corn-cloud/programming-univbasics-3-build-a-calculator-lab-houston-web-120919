798c20a09e3e44d4b19a543a4303b3f09e0d90b6
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2019-11-18 23:54:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,14 +6,24 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  num1 = 10 
+  num2 = 5 
+  return (10 - 5)
 
 end
 
 def division(num1, num2)
+  num1 = 50 
+  num2 = 2 
+  return (50 / 2)
 
 end
 
 def multiplication(num1, num2)
+  num1 = 4 
+  num2 = 30 
+  return (4 * 30)
+
 
 end
 
@@ -22,5 +32,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
+  
 
 end
